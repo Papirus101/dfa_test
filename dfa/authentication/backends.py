bfa069c91680dff5124e307ca8ab15a8baa01433
Ilya <kirilovilya@mail.ru>
2022-08-30 14:54:22 +0300

    add user auth

---
+++ dfa/authentication/backends.py
@@ -0,0 +1,56 @@
+import jwt
+
+from django.conf import settings
+
+from rest_framework import authentication, exceptions
+
+from .models import User
+
+
+class JWTAuthentication(authentication.BaseAuthentication):
+    authentication_header_prefix = 'Token'
+
+    def authenticate(self, request):
+        request.user = None
+
+        auth_header = request.headers.get('Authorization', None)
+        if auth_header is None:
+            return None
+        auth_header = auth_header.split()
+        auth_header_prefix = self.authentication_header_prefix.lower()
+        if not auth_header:
+            return None
+
+        if len(auth_header) == 1:
+            return None
+
+        elif len(auth_header) > 2:
+            return None
+
+        prefix = auth_header[0]
+        token = auth_header[1]
+
+        if prefix.lower() != auth_header_prefix:
+            return None
+
+        return self._authenticate_credentials(request, token)
+
+    def _authenticate_credentials(self, request, token):
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
+        except Exception as e:
+            print(e)
+            msg = 'Ошибка аутентификации. Невозможно декодировать токеню'
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            user = User.objects.get(pk=payload['id'])
+        except User.DoesNotExist:
+            msg = 'Пользователь соответствующий данному токену не найден.'
+            raise exceptions.AuthenticationFailed(msg)
+
+        if not user.is_active:
+            msg = 'Данный пользователь деактивирован.'
+            raise exceptions.AuthenticationFailed(msg)
+
+        return (user, token)
